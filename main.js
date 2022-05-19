d4a3b4c1a51c2ec2a98b0bb8d247fb4cf5852a2b
Jhalliwel <76626722+JHalliwel@users.noreply.github.com>
2022-05-18 21:13:03 -0400

    Add files via upload
    new

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=2nbQg0XiqItY0fh9aHPbEOAt2yyMPjyhqujIyJDD")
+fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=gWlocK7QkchUzLF522ui8j6svje1FDXpWbRPcan5")
     .then(res => res.json())
     .then(data => {
         console.log(data.photos)
